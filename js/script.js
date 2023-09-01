11bd3c49f39ba1ad48d2eb0dc1dc0f6d9e2e8e63
MaruBianchi <maribianchiro@gmail.com>
2023-09-01 01:28:25 -0300

    Se añaden comentarios guía al Fetch agregado

--- js/script.js
+++ js/script.js
@@ -19,34 +19,47 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+// Fetch recibe el URL y las opciones (en este caso, get, para obtener los datos del JSON)
 fetch(DATA_URL, {
   method: 'GET'
+  //Fetch genera una promesa de respuesta "response", con la información en formato de objeto
 }).then(response => {
 
+  // Se verifica que la respuesta haya sido válida
   if(!response.status === 200 || response.ok === false) {
 
+    // En caso de que suceda algún error con la solicitud en el Fetch, se muestra un alert y se cancela la ejecución
     alert("Error al procesar la solicitud");
 
     return;
 
   };
 
+  // Se verifican los datos recibidos en la respuesta del Fetch
   console.log(response);
 
+  // Se genera una nueva promesa para transformar los datos recibidos a JSON
   response.json().then(jsonRecibido =>{
 
+    // Una vez recibidos los datos, se verifica que exita un campo "students" con datos.
     if(!jsonRecibido.students) {
 
+      // En caso de que "students" no exista o no contenga información, se muestra un alert
       alert("No se encontraron los estudiantes")
 
+      // También se muestran en consola todos los datos que contiene el JSON recibido, para poder verificar qué se está recibiendo
       console.log(jsonRecibido);
 
     };
 
+    // Se crea una constante para utilizar el mismo nombre de variable como parámetro.
     const dataArray = jsonRecibido.students;
 
+    // Se ejecuta la función para mostrar los nombres de los estudiantes.
     showData(dataArray);
 
   })
 
+  // Se crea un catch como contról de errores.
 }).catch(error => error);
